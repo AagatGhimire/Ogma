0ceb6ca35cc5f8cf5586d8febcedcf6f31aca98f
Aagat Ghimire <aagat.gh001@gmail.com>
2023-05-13 16:47:05 +0545

    Routes Cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ogma/constants/routes.dart';
 import 'package:ogma/view/login_view.dart';
 import 'package:ogma/view/register_view.dart';
 import 'package:ogma/view/verify_email_view.dart';
@@ -17,9 +18,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/' : (context) => const NotesView(),
+        loginRoute : (context) => const LoginView(),
+        registerRoute : (context) => const RegisterView(),
+        notesRoute : (context) => const NotesView(),
       },
     ),
   );
@@ -82,7 +83,7 @@ class _NotesViewState extends State<NotesView> {
                   if (shouldLogout){
                     await FirebaseAuth.instance.signOut();
                     Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/login/', 
+                      loginRoute, 
                       (_) => false,
                       );
                   }
