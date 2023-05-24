ad3a5e401b610cbb119f6c0df5ba070bb9ecd92a
unknown <aagat.gh001@gmail.com>
2023-05-24 12:10:38 +0545

    Unit test inilialized

---
+++ test/auth_test.dart
@@ -0,0 +1,58 @@
+import 'package:ogma/services/auth/auth_provider.dart';
+import 'package:ogma/services/auth/auth_user.dart';
+import 'package:test/test.dart';
+
+void main() {}
+
+class NotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!_isInitialized) throw NotInitializedException();
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> initialize() {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
